5538505efcc48f859fc1cba20cbd600291e4dcf9
VictorAGM <143540484+VictorAGM@users.noreply.github.com>
2025-02-23 18:37:46 -0600

    Sobre print

---
+++ 001_Sobre'Print()'.py
@@ -0,0 +1,34 @@
+
+print("Hola Mundo")
+print("Víctor Adalberto García Medina")
+print("Hola me gusta")
+print()
+print("saltar lineas")
+print("Hola no me gusta\nsaltar lineas")
+print("Hola","me gusta","usar varios argumentos")
+
+#argumentos de print
+
+print("Soy un argumento", end="===")
+print()
+print("Soy otro argumento",end="")
+print("Voy a aparecer al lado del otro argumento")
+print("Me", "gusta","usar","separaciones",sep="+-+")
+
+#jugando con print
+
+print("    *"*2)
+print()
+print("   * *"*2)
+print()
+print(" *     *"*2)
+print()
+print("***   ***"*2)
+print()
+print("  *   *"*2)
+print()
+print("  *   *"*2)
+print()
+print("  *****"*2)
+
+print("    *\n   * *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
