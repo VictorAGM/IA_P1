f7a71476413ac34d2803bf288202f88469b7d92a
VictorAGM <143540484+VictorAGM@users.noreply.github.com>
2025-02-25 10:35:45 -0600

    Operadores en python

---
+++ 003_SobreOperadores.py
@@ -0,0 +1,21 @@
+#Suma y resta
+print(3+4)
+print(3-4.)
+#Multiplicacion y division
+print(3*4) 
+print(3/4)
+#Potenciacion
+print(3**4)
+print(2 ** 2.)
+#Modulo
+print(3%4)
+#Division entera
+print(5//4)
+print(5.//4)
+#Prioridad de operaciones
+print(3+4*5)
+print(4%3**2-2)
+print(3*1-5//2)
+#parentesis
+print((3+4)*5)
+print(3+(4*5))
