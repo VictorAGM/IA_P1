6b18f7da5f31d638f79c7e8eea4d8385adc81215
VictorAGM <143540484+VictorAGM@users.noreply.github.com>
2025-02-25 10:16:51 -0600

    Practica 2

---
+++ 002_SobreLiterales.py
@@ -0,0 +1,16 @@
+
+# Numeros Octales
+print(0o123)
+#Numeros hexadecimales
+print(0x123)
+#cadenas
+print("Hola Mundo")
+print('"Estoy programando en python"')
+print("007")
+#Flotantes y enteros
+print(3)
+print(3.34e-6)
+print(3.34e6)
+#booleanos
+print(True<False)
+print(False<True)
